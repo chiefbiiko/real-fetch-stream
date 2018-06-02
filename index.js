1e875962f09697f938db98abc718aeabdd3bb49d
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2018-06-02 16:28:43 +0200

    not rily duplexin

--- index.js
+++ index.js
@@ -1,70 +1,88 @@
 const { Duplex, Readable } = require('stream')
+const debug = require('debug')('real-fetch-stream')
 
 class Reader extends Readable {
+
   constructor (reader, opts = {}) {
     super(Object.assign(opts, { objectMode: false })) // rily?
     this._reader = reader
   }
+
   _read () {
     var self = this
     self._reader.read()
       .then(chunk => {
-        if (chunk.done) {
-          self.push(null)
-        } else {
-          const more = self.push(chunk.value)
-          if (more) self._read()
-        }
+        if (chunk.done) self.push(null)
+        else if (self.push(chunk.value)) self._read()
       })
       .catch(self.emit.bind(self, 'error'))
   }
+
 }
 
 class Duplo extends Duplex {
+
   constructor (url, opts = {}) {
+    super(Object.assign(opts, { objectMode: false })) // rily?
+    this._url = url
+    this._opts = opts
     this._chunks = []
   }
-  read (size) {
-    if (this._response === undefined) {
+
+  _read (size) {
+    debug('::readin::')
+    var self = this
+    if (self._response === undefined) {
+      debug('::response still undefined::')
       return
-    } else if (this._response) {
-      
-    } else if (this._response === null) {
-      this.push(null)
+    } else if (self._response === null) {
+      debug('::response is null::')
+      self.push(null)
+    } else if (self._response) {
+      debug('::response is truthy::')
+      self._response.body.read()
+        .then(chunk => {
+          if (chunk.done) self.push(null)
+          else if (self.push(chunk.value)) self._read()
+        })
+        .catch(self.emit.bind(self, 'error'))
     }
   }
-  write (chunk, enc, next) {
+
+  _write (chunk, enc, next) {
     this._chunks.push(chunk)
     next()
   }
-  final (end) {
+
+  _final (end) {
     var self = this
-    fetch(url, Object.assign(opts, { body: Buffer.concat(this._chunks) }))
-      .then(res => { self._response = res }) // ..?
-      .catch(reject)
+    fetch(self._url, Object.assign(self._opts, {
+      body: Buffer.concat(self._chunks)
+    }))
+      .then(res => {
+        if (!res.ok) self.emit('error')
+        debug('res::', res)
+        self._response = res
+      })
+      .catch(self.emit.bind(self, 'error'))
   }
+
 }
 
 function realFetchStream (url, opts) {
-  opts = Object.assign(opts || {}, {/*musthaves*/})
+  opts = Object.assign({ method: 'get' }, opts || {})
   return new Promise((resolve, reject) => {
     if (opts.method.toLowerCase() === 'get') {
       fetch(url, opts)
         .then(res => resolve(new Reader(res.body.getReader(), opts)))
         .catch(reject)
     } else if (opts.method.toLowerCase() === 'post') {
-      // return a duplex; writing post data, reading response data
       resolve(new Duplo(url, opts))
-      // resolve(concat(buf => { // writable only, want a duplex...
-      //   fetch(url, Object.assign(opts, { body: buf }))
-      //     .then(() => {}) // ???
-      //     .catch(reject) // ??
-      // }))
     } else {
       reject(new Error('unsupported HTTP method: ' + opts.method))
     }
   })
 }
 
-window.realFetchStream = realFetchStream
-// module.exports = realFetchStream
+module.exports = realFetchStream
+// window.realFetchStream = realFetchStream // 4 bundlin only
--- test.js
+++ test.js
@@ -1,12 +1,26 @@
 var realFetchStream = require('./index')
 var tape = require('tape')
 
-tape('node stream emits data', async function (t) {
-  var rfs = await realFetchStream('http://www.linux-usb.org/usb.ids')
+tape('get', async function (t) {
+  var endpoint = 'https://api.github.com/users'
+  var rfs = await realFetchStream(endpoint)
   rfs.on('data', function (chunk) {
     t.ok(chunk.length, 'got sth')
   })
   rfs.on('end', function () {
     t.end()
   })
+})
+
+tape('post', async function (t) {
+  var endpoint = 'https://jsonplaceholder.typicode.com/posts'
+  var rfs = await realFetchStream(endpoint, { method: 'post' })
+  rfs.on('data', function (chunk) {
+    t.ok(chunk.length, 'got some response')
+  })
+  rfs.on('end', function () {
+    t.end()
+  })
+  rfs.write('hello server')
+  rfs.end('!')
 })
