e8b8fbc88995b345ebe4a15f0752d780b79e6fe1
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2018-06-03 21:41:56 +0200

    mvp streamin

--- index.js
+++ index.js
@@ -3,13 +3,17 @@ const toEmitter = require('./promise-to-emitter')
 const debug = require('debug')('real-fetch-stream')
 debug.enabled = true
 
+/* TODO:
++ think about mime types and allowing objectMode
++ emit response headers
++ readme
+*/
+
 class ReaderWrapper extends Readable {
 
   constructor (url, opts = {}) {
     opts = Object.assign(Object.assign({}, opts), { objectMode: false }) // rily
     super(opts)
-    this._opts = opts
-    // this._ready = false
     this._reader = null
     this._fetcher = toEmitter(fetch(url, opts))
     this._fetcher.once('resolved', res => this._reader = res.body.getReader())
@@ -17,7 +21,6 @@ class ReaderWrapper extends Readable {
   }
 
   _read () {
-    // if (isPending(this._reader)) return this._reader.then(() => this._read())
     if (!this._reader) {
       return this._fetcher.once('resolved', this._read.bind(this))
     }
@@ -47,21 +50,20 @@ class DuplexWrapper extends Transform {
   }
 
   _flush (cb) {
-    var self = this
-    fetch(self._url, Object.assign(self._opts, {
-      body: Buffer.concat(self._chunks)
+    fetch(this._url, Object.assign(this._opts, {
+      body: Buffer.concat(this._chunks)
     }))
       .then(res => {
-        if (!res.ok) return self.emit('error')
+        if (!res.ok) return this.emit('error')
         const _reader = res.body.getReader()
         const _read = () => {
           _reader.read()
             .then(chunk => {
               if (chunk.done) {
-                self.push(null)
+                this.push(null)
                 _reader.releaseLock()
                 return cb()
-              } else if (self.push(chunk.value)) {
+              } else if (this.push(chunk.value)) {
                 return _read()
               }
             })
@@ -74,16 +76,12 @@ class DuplexWrapper extends Transform {
 
 }
 
-function realFetchStream (url, opts) {
+const realFetchStream = (url, opts) => {
   opts = Object.assign({ method: 'get' }, opts || {})
-  if (opts.method.toLowerCase() === 'get') {
-    return new ReaderWrapper(url, opts)
-  } else if (opts.method.toLowerCase() === 'post') {
-    return new DuplexWrapper(url, opts)
-  } else {
-    throw new Error('unsupported HTTP method: ' + opts.method)
-  }
+  const method = opts.method.trim().toLowerCase()
+  if (method === 'get') return new ReaderWrapper(url, opts)
+  else if (method === 'post') return new DuplexWrapper(url, opts)
+  else throw new Error('unsupported HTTP method: ' + method)
 }
 
-// module.exports = realFetchStream
-window.realFetchStream = realFetchStream // 4 bundlin only
+module.exports = realFetchStream
--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 var realFetchStream = require('./index')
 var tape = require('tape')
 
-tape.only('get', function (t) {
+tape('get', function (t) {
   var rfs = realFetchStream('https://api.github.com/users')
   rfs.on('data', function (chunk) {
     t.ok(chunk.length, 'got sth')
@@ -11,20 +11,9 @@ tape.only('get', function (t) {
   })
 })
 
-// tape.only('get', async function (t) {
-//   var endpoint = 'https://api.github.com/users'
-//   var rfs = await realFetchStream(endpoint)
-//   rfs.on('data', function (chunk) {
-//     t.ok(chunk.length, 'got sth')
-//   })
-//   rfs.on('end', function () {
-//     t.end()
-//   })
-// })
-
-tape('post', async function (t) {
+tape('post', function (t) {
   var endpoint = 'https://jsonplaceholder.typicode.com/posts'
-  var rfs = await realFetchStream(endpoint, { method: 'post' })
+  var rfs = realFetchStream(endpoint, { method: 'post' })
   rfs.on('data', function (chunk) {
     t.ok(chunk.length, 'got some response')
   })
