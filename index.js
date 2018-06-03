507c6efe0d5e99381f2f7a1057a53bf21a68b089
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2018-06-03 07:57:06 +0200

    as transform

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-const { Duplex, Readable } = require('stream')
+const { Duplex, Readable, Transform } = require('stream')
 const debug = require('debug')('real-fetch-stream')
 
-// TODO: reimplement Duplo via a Transform
+// TODO: reimplement DuplexWrapper via a Transform
 
 debug.enabled = true
 
-class Reader extends Readable {
+class ReaderWrapper extends Readable {
 
   constructor (reader, opts = {}) {
-    super(Object.assign(opts, { objectMode: false })) // rily?
+    opts = Object.assign(Object.assign({}, opts), { objectMode: false }) // rily
+    super(opts)
+    this._opts = opts
     this._reader = reader
     this.once('end', () => this._reader.releaseLock())
   }
@@ -25,70 +27,110 @@ class Reader extends Readable {
 
 }
 
-class Duplo extends Duplex {
+class DuplexWrapper extends Transform {
 
-  constructor (url, opts = {}) {
-    super(Object.assign(opts, { objectMode: false })) // rily?
+  constructor (url, opts) {
+    opts = Object.assign(Object.assign({}, opts), { objectMode: false }) // rily
+    super(opts)
     this._url = url
     this._opts = opts
     this._chunks = []
   }
 
-  _read (size) {
-    debug('::readin::')
-    var self = this
-    if (!self._response) {
-      debug('::response still undefined::')
-      // return
-      return self.once('_response', () => {
-        self._response.body.read()
-          .then(chunk => {
-            if (chunk.done) self.push(null)
-            else if (self.push(chunk.value)) self._read()
-          })
-          .catch(self.emit.bind(self, 'error'))
-      })
-    } else if (self._response) {
-      debug('::response is truthy::')
-      self._response.body.read()
-        .then(chunk => {
-          if (chunk.done) self.push(null)
-          else if (self.push(chunk.value)) self._read()
-        })
-        .catch(self.emit.bind(self, 'error'))
-    }
-  }
-
-  _write (chunk, enc, next) {
+  _transform (chunk, enc, next) {
     this._chunks.push(chunk)
     next()
   }
 
-  _final (end) {
+  _flush (cb) {
     var self = this
     fetch(self._url, Object.assign(self._opts, {
       body: Buffer.concat(self._chunks)
     }))
       .then(res => {
         if (!res.ok) self.emit('error')
-        self._response = res
-        self.emit('_response')
-        end()
+        const _read = () => {
+          res.body.read()
+            .then(chunk => {
+              if (chunk.done) {
+                self.push(null)
+                cb()
+              } else if (self.push(chunk.value)) {
+                _read()
+              }
+            })
+            .catch(self.emit.bind(self, 'error'))
+        }
       })
       .catch(self.emit.bind(self, 'error'))
   }
 
 }
 
+// class DuplexWrapper extends Duplex {
+//
+//   constructor (url, opts = {}) {
+//     super(Object.assign(opts, { objectMode: false })) // rily?
+//     this._url = url
+//     this._opts = opts
+//     this._chunks = []
+//   }
+//
+//   _read (size) {
+//     debug('::readin::')
+//     var self = this
+//     if (!self._response) {
+//       debug('::response still undefined::')
+//       // return
+//       return self.once('_response', () => {
+//         self._response.body.read()
+//           .then(chunk => {
+//             if (chunk.done) self.push(null)
+//             else if (self.push(chunk.value)) self._read()
+//           })
+//           .catch(self.emit.bind(self, 'error'))
+//       })
+//     } else if (self._response) {
+//       debug('::response is truthy::')
+//       self._response.body.read()
+//         .then(chunk => {
+//           if (chunk.done) self.push(null)
+//           else if (self.push(chunk.value)) self._read()
+//         })
+//         .catch(self.emit.bind(self, 'error'))
+//     }
+//   }
+//
+//   _write (chunk, enc, next) {
+//     this._chunks.push(chunk)
+//     next()
+//   }
+//
+//   _final (end) {
+//     var self = this
+//     fetch(self._url, Object.assign(self._opts, {
+//       body: Buffer.concat(self._chunks)
+//     }))
+//       .then(res => {
+//         if (!res.ok) self.emit('error')
+//         self._response = res
+//         self.emit('_response')
+//         end()
+//       })
+//       .catch(self.emit.bind(self, 'error'))
+//   }
+//
+// }
+
 function realFetchStream (url, opts) {
   opts = Object.assign({ method: 'get' }, opts || {})
   return new Promise((resolve, reject) => {
     if (opts.method.toLowerCase() === 'get') {
       fetch(url, opts)
-        .then(res => resolve(new Reader(res.body.getReader(), opts)))
+        .then(res => resolve(new ReaderWrapper(res.body.getReader(), opts)))
         .catch(reject)
     } else if (opts.method.toLowerCase() === 'post') {
-      resolve(new Duplo(url, opts))
+      resolve(new DuplexWrapper(url, opts))
     } else {
       reject(new Error('unsupported HTTP method: ' + opts.method))
     }
